0ae7e26fed933f6a5ce748a51085551f1a4db1d2
koala5656 <chorus27nao@gmail.com>
2024-02-17 23:43:21 +0900

    Itemモデルの単体テストコード

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_new, except: [:index]
+
   def index
     @items = Item.all
   end
@@ -7,9 +9,24 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to '/'     
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:title, :category, :summary, :condition, :shipping, :prefecture, :delivery_time, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_new
+    unless user_signed_in?
+      redirect_to user_session_path
+    end
   end
 end
